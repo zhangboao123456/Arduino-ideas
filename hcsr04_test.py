bec36971a3b1bace80973c2aff868192009f130d
zhangboao123456 <52529249+zhangboao123456@users.noreply.github.com>
2019-07-16 21:07:57 +0800

    Add files via upload

---
+++ hcsr04_test.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+
+Trig_Pin = 2
+Echo_Pin = 3
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(Trig_Pin, GPIO.OUT, initial = GPIO.LOW)
+GPIO.setup(Echo_Pin, GPIO.IN)
+
+time.sleep(2)
+
+def checkdist():
+    GPIO.output(Trig_Pin, GPIO.HIGH)
+    time.sleep(0.00015)
+    GPIO.output(Trig_Pin, GPIO.LOW)
+    while not GPIO.input(Echo_Pin):
+        pass
+    t1 = time.time()
+    while GPIO.input(Echo_Pin):
+        pass
+    t2 = time.time()
+    return (t2-t1)*340*100/2
+
+try:
+    while True:
+        print ('Distance:%0.2f cm' % checkdist())
+        time.sleep(1)
+except KeyboardInterrupt:
+    GPIO.cleanup()
